fb9664727ef2d2d3051edf423362d3e1ac65778c
wyhong3103 <wyhong3103@gmail.com>
2023-07-15 19:31:32 +0800

    Remove auto fill style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import './styles/style.css';
 import reportWebVitals from './reportWebVitals';
 
 import { extendTheme, ChakraProvider } from '@chakra-ui/react';
