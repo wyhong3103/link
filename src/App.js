93f23d89bc6f48fffeb726a424251b27d7089100
wyhong3103 <wyhong3103@gmail.com>
2023-07-15 18:23:00 +0800

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,31 @@
-export const App = () => {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { Verify } from "./pages/Verify";
+import { ResetPassword } from "./pages/ResetPassword";
+import { Home } from "./pages/Home";
+import { Profile } from "./pages/Profile";
+import { UpdateProfile } from "./pages/UpdateProfile";
+import { Users } from "./pages/Users";
+import { Friends } from "./pages/Friends";
+import { Chat } from "./pages/Chat";
 
+
+export const App = () => {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path='/login' element={<Login/>}/>
+                <Route path='/register' element={<Register/>}/>
+                <Route path='/verify' element={<Verify/>}/>
+                <Route path='/reset-password' element={<ResetPassword/>}/>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/profile/:id' element={<Profile/>}/>
+                <Route path='/update' element={<UpdateProfile/>}/>
+                <Route path='/users' element={<Users/>}/>
+                <Route path='/friends' element={<Friends/>}/>
+                <Route path='/chat' element={<Chat/>}/> 
+            </Routes>
+        </BrowserRouter>
+    )
 };
