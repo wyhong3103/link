5e61caafd5d77e0979ec283c86f97b815b8ad4b2
wyhong3103 <wyhong3103@gmail.com>
2023-07-15 21:58:36 +0800

    Add email verification page

---
+++ src/pages/Success.js
@@ -0,0 +1,25 @@
+import { Flex, VStack, Button,Text, Heading } from "@chakra-ui/react"
+
+export const Success = ({text}) => {
+    return(
+        <Flex bg='palette.4' minH={'100vh'} justify='center' align='center'>
+            <VStack p='30px' bg='palette.3' borderRadius='10px' gap='30px'>
+                <Heading color='palette.5'>
+                    Link
+                </Heading>
+                <Text color='palette.1' fontSize='17px'>
+                    {text}
+                </Text>
+                <Button bg='palette.4' minW='200px'
+                    css={{
+                        '&:hover': {
+                            backgroundColor: '#11999E',
+                        },
+                    }}
+                >
+                    <Text color='palette.1'>Login</Text>
+                </Button>
+            </VStack>
+        </Flex> 
+    )
+}
--- src/pages/Verify.js
+++ src/pages/Verify.js
@@ -1,3 +1,20 @@
-export const Verify = () => {
+import { Success } from "./Success"
+import { useState } from "react"
+import { Box } from "@chakra-ui/react";
 
+export const Verify = () => {
+    /*
+
+        Check if token return OK, if ok show success page, else show error
+
+    */
+
+    const [ok, setOk] = useState(true);
+    return(
+        ok ? 
+        <Success text="You are verified."/>
+        :
+        <Box bg='palette.4' minH = '100vh'>
+        </Box>
+    )
 }
