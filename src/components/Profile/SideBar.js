fb21d041f49e81a06f5ca461f72e3f36aaa6e676
wyhong3103 <wyhong3103@gmail.com>
2023-07-16 16:00:52 +0800

    Add profile page & its components

--- src/components/Profile/SideBar.js
+++ src/components/Profile/SideBar.js
@@ -1,59 +1,78 @@
-import {VStack, Button, Image, Text} from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+import {VStack, Button, Image, Text, HStack, Heading, Link} from '@chakra-ui/react';
 
-export const SideBar = ({url}) => {
+export const SideBar = ({url, type, id, friends}) => {
+    const navigate = useNavigate();
+
+    const go = (url) => {
+        navigate(url);
+    }
+
+    const link = (id) => {
+        // deal with API
+    }
+
+    const unlink = (id) => {
+        // deal with API
+    }
+
+    const barType = {
+        self : [
+            ['Update Profile', () => go('/update')]
+        ],
+        friend : [
+            ['Unlink', () => unlink(id)],
+            ['Chat', () => go('/chat?friendid='+ id)]
+        ],
+        stranger : [
+            ['Link', () => link(id)]
+        ]
+    }
+
     return (
-        <VStack w='300px'p='20px' bg='palette.3' borderRadius='10px' gap='20px'>
-            <VStack>
-                <Image src={url} alt='avatar' borderRadius='100%' w='150px'/>
-                <Text color='palette.1' fontWeight='800'>
-                    James
-                </Text>
+        <VStack>
+            <VStack w='300px'p='20px' borderRadius='10px' gap='20px'>
+                <VStack>
+                    <Image src={url} alt='avatar' borderRadius='100%' w='150px'/>
+                    <Text color='palette.1' fontWeight='800'>
+                        James
+                    </Text>
+                </VStack>
+                <VStack w='80%'>
+                    {
+                        barType[type].map(
+                            i => 
+                            <Button border='none' bg='palette.2' w='100%'
+                                css={{
+                                    '&:hover': {
+                                        backgroundColor: '#11999E',
+                                    },
+                                }}
+                                onClick={i[1]}
+                            >
+                                <Text color='palette.1'>
+                                    {i[0]}
+                                </Text>
+                            </Button>
+                        )
+                    }
+                </VStack>
             </VStack>
-            <VStack w='80%'>
-                <Button border='none' bg='palette.2' w='100%'
-                    css={{
-                        '&:hover': {
-                            backgroundColor: '#222831',
-                        },
-                    }}
-                >
-                    <Text color='palette.1'>
-                        Profile
-                    </Text>
-                </Button>
-                <Button border='none' bg='palette.2' w='100%'
-                    css={{
-                        '&:hover': {
-                            backgroundColor: '#222831',
-                        },
-                    }}
-                >
-                    <Text color='palette.1'>
-                        Chat
-                    </Text>
-                </Button>
-                <Button border='none' bg='palette.2' w='100%'
-                    css={{
-                        '&:hover': {
-                            backgroundColor: '#222831',
-                        },
-                    }}
-                >
-                    <Text color='palette.1'>
-                        Friends
-                    </Text>
-                </Button>
-                <Button border='none' bg='palette.2' w='100%'
-                    css={{
-                        '&:hover': {
-                            backgroundColor: '#222831',
-                        },
-                    }}
-                >
-                    <Text color='palette.1'>
-                        Link Up
-                    </Text>
-                </Button>
+            <VStack bg='palette.3' p='20px' borderRadius='10px' w='300px' gap='20px'>
+                <Heading fontSize='25px' color='palette.1' textAlign='left' w='100%'>
+                    Friends
+                </Heading>
+                {
+                    friends.map(
+                        i => 
+                        <HStack w='100%' gap='20px'>
+                            <Image src={i.image} alt='avatar' w='50px' objectFit='cover' borderRadius='100%'/>
+                            <Link path={`/profile/${i._id}`} color='palette.1' fontSize='20px'>
+                                {`${i.first_name} ${i.last_name}`}
+                            </Link>
+                        </HStack>
+                    )
+                }
             </VStack>
         </VStack>
     )
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,3 +1,70 @@
-export const Profile = () => {
+import { Flex, VStack } from "@chakra-ui/react"
+import { SideBar } from "../components/Profile/SideBar"
+import { Post } from "../components/Post"
+import { Nav } from "../components/Nav"
 
+export const Profile = () => {
+    return(
+        <VStack minH='100vh' bg='palette.4'>
+            <Nav/>
+            <Flex justify='space-around' w='100%' p={{base : '5px', md : '20px'}} align='start'>
+                <SideBar url="https://static.vecteezy.com/system/resources/previews/001/840/618/original/picture-profile-icon-male-icon-human-or-people-sign-and-symbol-free-vector.jpg"
+                    type='self'
+                    friends={
+                        [
+                            {
+                                first_name : "Barry",
+                                last_name : "Allen",
+                                image : "https://static.vecteezy.com/system/resources/previews/001/840/618/original/picture-profile-icon-male-icon-human-or-people-sign-and-symbol-free-vector.jpg",
+                                _id : "test"
+                            },
+                            {
+                                first_name : "Barry",
+                                last_name : "Allen",
+                                image : "https://static.vecteezy.com/system/resources/previews/001/840/618/original/picture-profile-icon-male-icon-human-or-people-sign-and-symbol-free-vector.jpg",
+                                _id : "test"
+                            }
+                        ]
+                    }   
+                />
+                <VStack>
+                    <Post
+                        post={
+                            {
+                                author : {
+                                    _id : 'test', 
+                                    first_name : 'Barry', 
+                                    last_name : 'Allen', 
+                                    image : 'https://static.vecteezy.com/system/resources/previews/001/840/618/original/picture-profile-icon-male-icon-human-or-people-sign-and-symbol-free-vector.jpg'
+                                } ,
+                                content : "Pellentesque ut iaculis lectus. Maecenas scelerisque fringilla massa id dapibus. Proin ullamcorper risus in orci faucibus aliquam. Nam accumsan porttitor lorem, et dignissim augue venenatis sed. Sed suscipit purus nunc, at hendrerit sapien gravida id. Suspendisse at lorem mollis, cursus mauris quis, aliquet lectus. Suspendisse efficitur hendrerit purus, vel interdum velit tincidunt eu. Vivamus sit amet malesuada felis, ac fermentum nisi. Maecenas porttitor nunc in semper egestas. Mauris et sem lacinia, ultrices ligula nec, volutpat diam. Donec mollis tempor elit, a accumsan urna porta in. Pellentesque non odio ac mi pulvinar convallis ac ut nisl. Vivamus.", 
+                                markdown : true, 
+                                math : true,
+                                date : '15 July 2023', 
+                                image : '',
+                                comments : 
+                                [ 
+                                    {
+                                        _id : 'test', 
+                                        content : "Pellentesque ut iaculis lectus. Maecenas scelerisque fringilla massa id dapibus. Proin ullamcorper risus in orci faucibus aliquam. Nam accumsan porttitor lorem, et dignissim augue venenatis sed. Sed suscipit purus nunc, at hendrerit sapien gravida id. Suspendisse at lorem mollis, cursus mauris quis, aliquet lectus. Suspendisse efficitur hendrerit purus, vel interdum velit tincidunt eu. Vivamus sit amet malesuada felis, ac fermentum nisi. Maecenas porttitor nunc in semper egestas. Mauris et sem lacinia, ultrices ligula nec, volutpat diam. Donec mollis tempor elit, a accumsan urna porta in. Pellentesque non odio ac mi pulvinar convallis ac ut nisl. Vivamus.", 
+                                        author : {
+                                            _id : 'test', 
+                                            first_name : "Barry", 
+                                            last_name : "Allen", 
+                                            image : 'https://static.vecteezy.com/system/resources/previews/001/840/618/original/picture-profile-icon-male-icon-human-or-people-sign-and-symbol-free-vector.jpg'
+                                        } ,
+                                        markdown : true,
+                                        math : true,
+                                        date : "15 July 2023"
+                                    } 
+                                ],
+                                likes : [1, 2, 3, 4, 5],
+                            }
+                        }
+                        
+                    />
+                </VStack> 
+            </Flex>
+        </VStack>
+    )
 }
