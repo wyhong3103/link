e2d55985042c797151af66e404b9a4a376f22a1f
wyhong3103 <wyhong3103@gmail.com>
2023-07-16 16:15:23 +0800

    Change friend panel to modal

--- src/components/Profile/SideBar.js
+++ src/components/Profile/SideBar.js
@@ -1,7 +1,18 @@
 import { useNavigate } from 'react-router-dom';
 import {VStack, Button, Image, Text, HStack, Heading, Link} from '@chakra-ui/react';
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure
+} from '@chakra-ui/react'
 
 export const SideBar = ({url, type, id, friends}) => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
     const navigate = useNavigate();
 
     const go = (url) => {
@@ -31,6 +42,28 @@ export const SideBar = ({url, type, id, friends}) => {
 
     return (
         <VStack>
+            <Modal isOpen={isOpen} onClose={onClose} scrollBehavior='inside'> 
+                <ModalOverlay />
+                <ModalContent bg='palette.3'>
+                    <ModalHeader color='palette.1'>Friends</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody >
+                        <VStack p='10px' borderRadius='10px' w='250px' gap='20px'>
+                            {
+                                friends.map(
+                                    i => 
+                                    <HStack w='100%' gap='20px'>
+                                        <Image src={i.image} alt='avatar' w='40px' objectFit='cover' borderRadius='100%'/>
+                                        <Link path={`/profile/${i._id}`} color='palette.1' fontSize='17px'>
+                                            {`${i.first_name} ${i.last_name}`}
+                                        </Link>
+                                    </HStack>
+                                )
+                            }
+                        </VStack>
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
             <VStack w='300px'p='20px' borderRadius='10px' gap='20px'>
                 <VStack>
                     <Image src={url} alt='avatar' borderRadius='100%' w='150px'/>
@@ -56,23 +89,19 @@ export const SideBar = ({url, type, id, friends}) => {
                             </Button>
                         )
                     }
+                    <Button border='none' bg='palette.2' w='100%'
+                        css={{
+                            '&:hover': {
+                                backgroundColor: '#11999E',
+                            },
+                        }}
+                        onClick={onOpen}
+                    >
+                        <Text color='palette.1'>
+                            Friends
+                        </Text>
+                    </Button>
                 </VStack>
-            </VStack>
-            <VStack bg='palette.3' p='20px' borderRadius='10px' w='300px' gap='20px'>
-                <Heading fontSize='25px' color='palette.1' textAlign='left' w='100%'>
-                    Friends
-                </Heading>
-                {
-                    friends.map(
-                        i => 
-                        <HStack w='100%' gap='20px'>
-                            <Image src={i.image} alt='avatar' w='50px' objectFit='cover' borderRadius='100%'/>
-                            <Link path={`/profile/${i._id}`} color='palette.1' fontSize='20px'>
-                                {`${i.first_name} ${i.last_name}`}
-                            </Link>
-                        </HStack>
-                    )
-                }
             </VStack>
         </VStack>
     )
