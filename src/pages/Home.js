3b20aaa828f925aea290161f2cf58d6f88855bf9
wyhong3103 <wyhong3103@gmail.com>
2023-07-17 13:25:54 +0800

    Add posts api methods

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,52 +1,69 @@
 import { Editor } from "../components/Editor"
-import { VStack, Flex, Box } from "@chakra-ui/react"
+import { VStack, Flex, Box, Spinner, Text } from "@chakra-ui/react"
 import { Nav } from "../components/Nav"
 import { Post } from "../components/Post"
+import { useEffect, useState } from "react"
+import useAuth from "../hooks/useAuth"
 
 export const Home = () => {
+    const [user] = useAuth()
+    const [loading, setLoading] = useState({});
+    const [blogs, setBlogs] = useState([]);
+    const api_url = process.env.REACT_APP_API_URL;
+
+    useEffect(
+        () => {
+            (async () => {
+                if (user){
+                    const res = await fetch(
+                        api_url + '/post',
+                        {
+                            credentials : 'include'
+                        }
+                    );
+
+                    const data = await res.json();
+
+                    setBlogs([...data.posts]);
+                    setLoading(false);
+                }
+            })()
+        }
+    , [user])
+
+
     return(
+        loading ?
+        
+        <Flex bg='palette.4' minH='100vh' justify='center' align='center'>
+            <Spinner
+            thickness='4px'
+            speed='0.65s'
+            emptyColor='gray.200'
+            color='palette.6'
+            size='xl'
+            />
+        </Flex>
+
+        :
         <VStack bg='palette.4' minH='100vh'>
-            <Nav/>
+            <Nav last_name={user.last_name} id={user.userid}/>
             <Flex p={{base : '5px', md : '20px'}} w='100%'>
                 <VStack w='100%' gap='30px'>
                     <Box w={{base : '400px', md :'700px'}} bg='palette.3' p='10px' borderRadius='10px'>
                         <Editor placeholder="Write a post."/>
                     </Box>
-                    <Post
-                        post={
-                            {
-                                author : {
-                                    _id : 'test', 
-                                    first_name : 'Barry', 
-                                    last_name : 'Allen', 
-                                    image : 'https://static.vecteezy.com/system/resources/previews/001/840/618/original/picture-profile-icon-male-icon-human-or-people-sign-and-symbol-free-vector.jpg'
-                                } ,
-                                content : "Pellentesque ut iaculis lectus. Maecenas scelerisque fringilla massa id dapibus. Proin ullamcorper risus in orci faucibus aliquam. Nam accumsan porttitor lorem, et dignissim augue venenatis sed. Sed suscipit purus nunc, at hendrerit sapien gravida id. Suspendisse at lorem mollis, cursus mauris quis, aliquet lectus. Suspendisse efficitur hendrerit purus, vel interdum velit tincidunt eu. Vivamus sit amet malesuada felis, ac fermentum nisi. Maecenas porttitor nunc in semper egestas. Mauris et sem lacinia, ultrices ligula nec, volutpat diam. Donec mollis tempor elit, a accumsan urna porta in. Pellentesque non odio ac mi pulvinar convallis ac ut nisl. Vivamus.", 
-                                markdown : true, 
-                                math : true,
-                                date : '15 July 2023', 
-                                image : '',
-                                comments : 
-                                [ 
-                                    {
-                                        _id : 'test', 
-                                        content : "Pellentesque ut iaculis lectus. Maecenas scelerisque fringilla massa id dapibus. Proin ullamcorper risus in orci faucibus aliquam. Nam accumsan porttitor lorem, et dignissim augue venenatis sed. Sed suscipit purus nunc, at hendrerit sapien gravida id. Suspendisse at lorem mollis, cursus mauris quis, aliquet lectus. Suspendisse efficitur hendrerit purus, vel interdum velit tincidunt eu. Vivamus sit amet malesuada felis, ac fermentum nisi. Maecenas porttitor nunc in semper egestas. Mauris et sem lacinia, ultrices ligula nec, volutpat diam. Donec mollis tempor elit, a accumsan urna porta in. Pellentesque non odio ac mi pulvinar convallis ac ut nisl. Vivamus.", 
-                                        author : {
-                                            _id : 'test', 
-                                            first_name : "Barry", 
-                                            last_name : "Allen", 
-                                            image : 'https://static.vecteezy.com/system/resources/previews/001/840/618/original/picture-profile-icon-male-icon-human-or-people-sign-and-symbol-free-vector.jpg'
-                                        } ,
-                                        markdown : true,
-                                        math : true,
-                                        date : "15 July 2023"
-                                    } 
-                                ],
-                                likes : [1, 2, 3, 4, 5],
-                            }
-                        }
-                        
-                    />
+                    {
+                        blogs.length > 0 ?
+                        blogs.map(
+                            i => 
+                            <Post post={i}/>
+                        )
+                        :
+                        <Text color='palette.1'>
+                            No posts! Make friends to see their posts here.
+                        </Text>
+                    }
                 </VStack>
             </Flex>
         </VStack>
