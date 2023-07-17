c660fba196676ea2848a81d5f9eab5122bf01502
wyhong3103 <wyhong3103@gmail.com>
2023-07-17 10:50:35 +0800

    Add error boundary

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+"use client";
+import { ErrorBoundary } from "react-error-boundary";
+import { Error } from "./pages/Error";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
@@ -17,21 +20,23 @@ import { Chat } from "./pages/Chat";
 export const App = () => {
     return(
         <BrowserRouter>
-            <Routes>
-                <Route path='/login' element={<Login/>}/>
-                <Route path='/register' element={<Register/>}/>
-                <Route path='/verify' element={<Verify/>}/>
-                <Route path='/reset-password' element={<ResetPassword/>}/>
-                <Route path='/' element={<Home/>}/>
-                <Route path='/search' element={<Search/>}/>
-                <Route path='/profile/:id' element={<Profile/>}/>
-                <Route path='/update' element={<UpdateProfile/>}/>
-                <Route path='/change-password' element={<ChangePassword/>}/>
-                <Route path='/users' element={<Users/>}/>
-                <Route path='/friends' element={<Friends/>}/>
-                <Route path='/friend-requests' element={<FriendRequests/>}/>
-                <Route path='/chat' element={<Chat/>}/> 
-            </Routes>
+            <ErrorBoundary FallbackComponent={Error}>
+                <Routes>
+                    <Route path='/login' element={<Login/>}/>
+                    <Route path='/register' element={<Register/>}/>
+                    <Route path='/verify' element={<Verify/>}/>
+                    <Route path='/reset-password' element={<ResetPassword/>}/>
+                    <Route path='/' element={<Home/>}/>
+                    <Route path='/search' element={<Search/>}/>
+                    <Route path='/profile/:id' element={<Profile/>}/>
+                    <Route path='/update' element={<UpdateProfile/>}/>
+                    <Route path='/change-password' element={<ChangePassword/>}/>
+                    <Route path='/users' element={<Users/>}/>
+                    <Route path='/friends' element={<Friends/>}/>
+                    <Route path='/friend-requests' element={<FriendRequests/>}/>
+                    <Route path='/chat' element={<Chat/>}/>
+                </Routes>
+            </ErrorBoundary>
         </BrowserRouter>
     )
 };
---
+++ src/pages/Error.js
@@ -0,0 +1,36 @@
+import { Flex, VStack, Heading, Text, Button } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
+
+export const Error = ({error, resetErrorBoundary}) => {
+    const navigate = useNavigate();
+
+    return(
+        <Flex minH='100vh' bg='palette.4' justify='center' align='center'>
+            <VStack p='20px' bg='palette.3' w='250px' borderRadius='10px' gap='20px'>
+                <Heading color='palette.1'>
+                    {error.status}
+                </Heading>
+                <Text color='palette.1'>
+                    {error.message}
+                </Text>
+                <Button bg='palette.4' minW='100%'
+                    css={{
+                        '&:hover': {
+                            backgroundColor: '#11999E',
+                        },
+                        '&:active': {
+                            backgroundColor: '#11999E',
+                        }
+                    }}
+                    onClick={() => {
+                        resetErrorBoundary()
+                        navigate('/')
+                    }}
+                    color='palette.1'
+                >
+                    Home
+                </Button>
+            </VStack>
+        </Flex>
+    )
+}
