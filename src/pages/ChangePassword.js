d31a588d4e84bae91d75a5b75f5a08436d1c137a
wyhong3103 <wyhong3103@gmail.com>
2023-07-17 23:55:32 +0800

    Add change password api methods

--- src/pages/ChangePassword.js
+++ src/pages/ChangePassword.js
@@ -6,9 +6,16 @@ import {
     Text, 
     Heading, 
     InputGroup, 
-    InputRightElement
+    InputRightElement,
+    Flex,
+    Spinner,
+    FormErrorMessage,
+    FormControl,
+    FormLabel
 } from "@chakra-ui/react"
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import useAuth from "../hooks/useAuth";
+import { useErrorBoundary } from "react-error-boundary";
 
 export const ChangePassword = () => {
     /*
@@ -16,14 +23,90 @@ export const ChangePassword = () => {
     Send to API
 
     */
+    const [ self ] = useAuth();
+    const { showBoundary } = useErrorBoundary();
+    const [loading, setLoading] = useState(true);
+    const [formData, setFormData] = useState({
+        old_password : '',
+        new_password : '',
+        new_repassword : ''
+    })
+    const [error, setError] = useState({
+        old_password : '',
+        new_password : '',
+        new_repassword : ''
+    })
     const [show1, setShow1] = useState(false)
     const [show2, setShow2] = useState(false)
     const [show3, setShow3] = useState(false)
+    const api_url = process.env.REACT_APP_API_URL;
+
+    const handleInputChange = (field, value) => {
+        const temp = {...formData};
+        temp[field] = value;
+        setFormData({...temp});
+    }
+
+    const submit = async () => {
+        const res = await fetch(
+            api_url + `/user/${self.userid}/password`,
+            {
+                method: 'PUT',
+                credentials : 'include',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            }
+        );
+        
+        const data = await res.json();
+
+        if (res.ok){
+            setFormData({
+                old_password : '',
+                new_password : '',
+                new_repassword : ''
+            })
+        } 
+        else if (res.status <= 402){
+            setError(data.error);
+        } else {
+            const error = new Error(data.error.result);
+            error.status = res.status;
+            error.isLogged = true;
+            showBoundary(error);
+        }
+    }
+
+    useEffect(
+        () => {
+            (async () => {
+                if (self){
+                    setLoading(false);
+                }
+            })()
+        }
+    , [self])
 
     return(
+        loading ?
+
+        <Flex bg='palette.4' minH='100vh' justify='center' align='center'>
+            <Spinner
+            thickness='4px'
+            speed='0.65s'
+            emptyColor='gray.200'
+            color='palette.6'
+            size='xl'
+            />
+        </Flex>
+
+        :
+
         <VStack minH='100vh' bg='palette.4'>
 
-            <Nav/>
+            <Nav id={self.userid} last_name={self.last_name}/>
 
             <VStack bg='palette.3' p='30px' borderRadius='10px' gap='20px' w='400px' margin='20px'> 
 
@@ -33,10 +116,10 @@ export const ChangePassword = () => {
 
                 <VStack gap='10px' w='100%'>
 
-                    <VStack w='100%'>
-                        <Text color='palette.1' w='100%'>
+                    <FormControl w='100%' isInvalid={error.old_password && error.old_password.length > 0}>
+                        <FormLabel color='palette.1' w='100%'>
                             Old Password
-                        </Text>
+                        </FormLabel>
                         <InputGroup>
                             <Input
                                 pr='4.5rem'
@@ -47,6 +130,8 @@ export const ChangePassword = () => {
                                     "border" : "none"
                                 }}
                                 color='palette.1'
+                                value={formData.old_password}
+                                onChange={(e) => handleInputChange('old_password', e.target.value)}
                             />
                             <InputRightElement width='4.5rem'>
                                 <Button h='1.75rem' size='sm' onClick={() => setShow1(!show1)} bg='palette.4'
@@ -63,12 +148,15 @@ export const ChangePassword = () => {
                                 </Button>
                             </InputRightElement>
                         </InputGroup>
-                    </VStack>
+                        <FormErrorMessage>
+                            {error.old_password}
+                        </FormErrorMessage>
+                    </FormControl>
 
-                    <VStack w='100%'>
-                        <Text color='palette.1' w='100%'>
+                    <FormControl w='100%' isInvalid={error.new_password && error.new_password.length > 0}>
+                        <FormLabel color='palette.1' w='100%'>
                             New Password
-                        </Text>
+                        </FormLabel>
                         <InputGroup>
                             <Input
                                 pr='4.5rem'
@@ -79,6 +167,8 @@ export const ChangePassword = () => {
                                     "border" : "none"
                                 }}
                                 color='palette.1'
+                                value={formData.new_password}
+                                onChange={(e) => handleInputChange('new_password', e.target.value)}
                             />
                             <InputRightElement width='4.5rem'>
                                 <Button h='1.75rem' size='sm' onClick={() => setShow2(!show2)} bg='palette.4'
@@ -95,12 +185,15 @@ export const ChangePassword = () => {
                                 </Button>
                             </InputRightElement>
                         </InputGroup>
-                    </VStack>
+                        <FormErrorMessage>
+                            {error.new_password}
+                        </FormErrorMessage>
+                    </FormControl>
 
-                    <VStack w='100%'>
-                        <Text color='palette.1' w='100%'>
+                    <FormControl w='100%' isInvalid={error.new_repassword && error.new_repassword.length > 0}>
+                        <FormLabel color='palette.1' w='100%'>
                             Confirm New Password
-                        </Text>
+                        </FormLabel>
                         <InputGroup>
                             <Input
                                 pr='4.5rem'
@@ -111,6 +204,8 @@ export const ChangePassword = () => {
                                     "border" : "none"
                                 }}
                                 color='palette.1'
+                                value={formData.new_repassword}
+                                onChange={(e) => handleInputChange('new_repassword', e.target.value)}
                             />
                             <InputRightElement width='4.5rem'>
                                 <Button h='1.75rem' size='sm' onClick={() => setShow3(!show3)} bg='palette.4'
@@ -127,7 +222,10 @@ export const ChangePassword = () => {
                                 </Button>
                             </InputRightElement>
                         </InputGroup>
-                    </VStack>
+                        <FormErrorMessage>
+                            {error.new_repassword}
+                        </FormErrorMessage>
+                    </FormControl>
 
                 </VStack>
 
@@ -141,6 +239,7 @@ export const ChangePassword = () => {
                                 backgroundColor: '#11999E',
                             },
                         }}
+                        onClick={submit}
                     >
                         <Text color='palette.1'>Submit</Text>
                     </Button>
