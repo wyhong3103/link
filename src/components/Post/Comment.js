f6fcb299c2537fe5cd9b8f24ec73999bc9ac7947
wyhong3103 <wyhong3103@gmail.com>
2023-07-17 19:12:54 +0800

    Update comment component

--- src/components/Post/Comment.js
+++ src/components/Post/Comment.js
@@ -94,14 +94,14 @@ export const Comment = ({comment, postid, fetchPosts, userid}) => {
             </Modal>
 
 
-            <Flex direction='row' w='100%' gap='20px'>
+            <Flex direction='row' w='100%' gap='20px' align='center'>
                 <Box>
                     <Image src={`${comment.author.image}` || anonymousImage} w='50px' objectFit='cover' borderRadius='100%'/>
                 </Box>
                 <VStack w='100%'> 
-                        <Flex direction='row' w='100%' justify='space-between'>
+                        <Flex direction='row' w='100%' justify='space-between' align='center'>
                             <Flex direction='row' w='100%'>
-                                <Link color='palette.1'>
+                                <Link color='palette.1' href={`/profile/${comment.author._id}`}>
                                     {`${comment.author.first_name} ${comment.author.last_name}` }
                                 </Link>
                                 <Text color='palette.1'>
