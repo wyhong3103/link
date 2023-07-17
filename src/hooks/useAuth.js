18af8b73764171ea0fb3fdbacbda06344b0ee52c
wyhong3103 <wyhong3103@gmail.com>
2023-07-17 20:32:31 +0800

    Redirect user to /login if not logged

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,9 +1,9 @@
-import { useErrorBoundary } from "react-error-boundary";
+import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 const useAuth = () => {
     const [data, setData] = useState(null);
-    const { showBoundary } = useErrorBoundary();
+    const navigate = useNavigate();
     const api_url = process.env.REACT_APP_API_URL;
 
     useEffect(
@@ -17,9 +17,7 @@ const useAuth = () => {
                 );
                 const statusData = await statusRes.json();
                 if (!statusRes.ok){
-                    const error = new Error('You are not logged in.');
-                    error.status = 403;
-                    showBoundary(error) ;
+                    navigate('/login');
                 }else{
                     setData({userid : statusData.userid, first_name : statusData.first_name, last_name : statusData.last_name})
                 }
